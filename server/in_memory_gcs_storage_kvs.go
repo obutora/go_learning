f6fb9aa6d2ac61a24fda9894cd7f5998168a57cc
HagaKun <obutora@users.noreply.github.com>
2024-01-01 11:36:52 +0000

    auto save when closing contianer

--- server/in_memory_gcs_storage_kvs.go
+++ server/in_memory_gcs_storage_kvs.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
@@ -11,13 +17,32 @@ import (
 	"github.com/minedia/orca-graphql-server/service"
 )
 
+const (
+	Bucket = "orca-cache"
+	Path   = "cache.json"
+)
+
+// In-memory data store.
+var m = map[string]interface{}{}
+
+// Create channel to listen for signals.
+var signalChan chan (os.Signal) = make(chan os.Signal, 1)
+
+func init() {
+	ctx := context.Background()
+	d, err := service.ReadFromGcs(ctx, Bucket, Path)
+	if err != nil {
+		fmt.Printf("failed to load from GCS: %v\n", err)
+	}
+	m = d
+}
+
+//NOTE: CloudRunのライフサイクル
+// https://cloud.google.com/blog/ja/products/serverless/lifecycle-container-cloud-run
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-
-	m := map[string]interface{}{
-		"a": "world",
-	}
 
 	r.Get("/read/{key}", func(w http.ResponseWriter, r *http.Request) {
 		key := chi.URLParam(r, "key")
@@ -59,43 +84,60 @@ func main() {
 			render.Status(r, http.StatusOK)
 			render.JSON(w, r, map[string]interface{}{
 				"status": "success",
+				"value":  value,
 			})
-			return
 		}
-		render.JSON(w, r, params)
 	})
 
-	r.Post("/save", func(w http.ResponseWriter, r *http.Request) {
-		d, err := json.Marshal(m)
-		if err != nil {
-			fmt.Printf("failed to marshal: %v\n", err)
-			render.Status(r, http.StatusInternalServerError)
-			render.JSON(w, r, map[string]interface{}{
-				"error": err.Error(),
-			})
-			return
-		}
+	go func() {
+		fmt.Printf("server is running on port %v\n", 8080)
+		http.ListenAndServe(":8080", r)
+	}()
 
-		err = service.UploadToGcs(r.Context(),
-			"orca-cache", // GCSのバケット名
-			"cache.json", // Storageに保存されるファイル名
-			d,
-		)
-		if err != nil {
-			render.Status(r, http.StatusInternalServerError)
-			render.JSON(w, r, map[string]interface{}{
-				"error": err.Error(),
-			})
-			return
-		}
-		render.Status(r, http.StatusOK)
-		render.PlainText(w, r, "success")
-	})
+	// SIGINT handles Ctrl+C locally.
+	// SIGTERM handles Cloud Run termination signal.
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
-	r.Get("/load", func(w http.ResponseWriter, r *http.Request) {
-		
-	})
+	// Receive output from signalChan.
+	sig := <-signalChan
+	log.Printf("%s signal caught", sig)
 
-	fmt.Printf("server is running on port %v\n", 8080)
-	http.ListenAndServe(":8080", r)
+	// Timeout if waiting for connections to return idle.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err := saveGcs(ctx)
+	if err != nil {
+		fmt.Printf("failed to save GCS: %v\n", err)
+	}
+
+}
+
+func saveGcs(ctx context.Context) error {
+	s := time.Now()
+
+	// GCSのデータとインメモリのデータをマージする
+	gcsData, err := service.ReadFromGcs(ctx, Bucket, Path)
+	if err != nil {
+		fmt.Printf("failed to load from GCS: %v\n", err)
+		return err
+	}
+
+	// GCSのデータをインメモリのデータで上書きする
+	for k, v := range m {
+		gcsData[k] = v
+	}
+
+	j, err := json.Marshal(gcsData)
+	if err != nil {
+		fmt.Printf("failed to marshal: %v\n", err)
+	}
+
+	err = service.UploadToGcs(ctx, Bucket, Path, j)
+	if err != nil {
+		fmt.Printf("failed to upload to GCS: %v\n", err)
+		return err
+	}
+	fmt.Printf("save gcs execution time: %v\n", time.Since(s))
+	return nil
 }
--- service/gcs_service.go
+++ service/gcs_service.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 
 	"cloud.google.com/go/storage"
@@ -36,4 +39,35 @@ func UploadToGcs(ctx context.Context, bucket, path string, data []byte) error {
 
 	log.Println("[GCS upload] file upload success")
 	return nil
+}
+
+func ReadFromGcs(ctx context.Context, bucket, path string) (map[string]interface{}, error) {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		logger.Error("[GCS upload] failed to create client", err)
+		return nil, err
+	}
+	// bucketオブジェクトの作成
+	b := client.Bucket(bucket)
+	// bucket内のオブジェクトの作成
+	obj := b.Object(path)
+
+	reader, err := obj.NewReader(ctx)
+	if err != nil {
+		fmt.Printf("Failed to create object reader: %v", err)
+		return nil, err
+	}
+	defer reader.Close()
+
+	d, err := io.ReadAll(reader)
+	if err != nil {
+		fmt.Printf("Failed to read object: %v", err)
+		return nil, err
+	}
+
+	var m map[string]interface{}
+	json.Unmarshal(d, &m)
+
+	fmt.Printf("Object contents: %s\n", d)
+	return m, nil
 }
